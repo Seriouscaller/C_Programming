a2527551866d3979716725c81db1349d98fcb8ba
Seriouscaller <30377011+Seriouscaller@users.noreply.github.com>
2024-03-14 23:07:34 +0100

    Refinement

--- Lab1/main.c
+++ Lab1/main.c
@@ -1,39 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 int main()
 {
-    int current_horizontal_column = 1;
+    int current_horizontal_column;
     int current_vertical_line = 1;
     int factor;
     int product = 0;
     int scanf_status_code;
     int user_answer;
+    bool validInput = false;
 
+    do{
+        printf("Enter the size of the table: ");
+        scanf_status_code = scanf("%d", &user_answer);
+        printf("Scanf statuscode: %d\n", scanf_status_code);
+        clearInputBuffer();
 
-    printf("Enter the size of the table: ");
-    scanf_status_code = scanf("%d", &user_answer);
-    printf("Scanf statuscode: %d\n", scanf_status_code);
+        //Make sure input is numeric between 1-10
+        if(scanf_status_code == 1 && user_answer <= 10 && user_answer >= 1){
+            validInput = true;
+        }
+    }while(!validInput);
+
     factor = user_answer;
-
 
     //TODO INPUT VALIDATION
 
-
     //Insert forloop into flowchart
-    //Formatting Header
-    printf(" X |");
-    for(current_horizontal_column; current_horizontal_column <= factor;
-                                                    current_horizontal_column++){
-        printf("%5.d", current_horizontal_column);
-    }
+    displayHeader(factor);
     printf("\n");
-    int bars = ()
-    for(int i = 0; i < ){}
-    printf("%s", bar * 5);
 
-
-    //
+    //Vertical-loop
+    for(current_vertical_line; current_vertical_line <= factor; current_vertical_line++){
+        createHorizontalLine(factor, current_vertical_line);
+        printf("\n");
+    }
     return 0;
 }
 
+void clearInputBuffer(void){
+    while ((getchar()) != '\n');
+    return;
+}
+
+void createHorizontalLine(int size, int vertical_linenumber){
+    int product;
+    int current_horizontal_column;
+
+    printf("%3.d |", vertical_linenumber);
+
+    for(current_horizontal_column = 1;current_horizontal_column <= size; current_horizontal_column++){
+        product = current_horizontal_column * vertical_linenumber;
+        printf("%5.d", product);
+    }
+    return;
+}
+
+void displayHeader(int size){
+    int current_factor = 1;
+
+    printf("  x |");
+    for(current_factor; current_factor <= size;
+                                   current_factor++){
+        printf("%5.d", current_factor);
+    }
+    printf("\n");
+    int bars = size * 5 + 5;
+    for(int i = 0; i < bars;i++){
+        printf("-");
+    }
+    return;
+}
