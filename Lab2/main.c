4753e4249eff72cdf0b27c7777d3d9376485b3f3
Seriouscaller <30377011+Seriouscaller@users.noreply.github.com>
2024-03-19 09:50:56 +0100

    More work

--- Lab2/main.c
+++ Lab2/main.c
@@ -10,6 +10,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
+float absoluteValue(float current_term);
+void displayRowResult(int term_number, float current_term);
+float calculateTerm(int x, int term_number);
+int calculateFactorial(int term);
+int power(int base, int exponent);
+void displayHeader();
+int readUserInputAndValidate(char prompt [], int lowerLimit, int upperLimit);
+void clearInputBuffer(void);
+
 #define DEBUG;
 
 #define NEWLINE '\012'
@@ -21,30 +31,43 @@ int main()
     int x;
     int n = 1;
     float latest_term;
-    //float current_term;
-
+    float current_term;
+    int term_number = 1;
+    bool is_limit_reached = false;
+    float sum = 1;
     const float THRESHOLD = 0.0000001;
 
     x = readUserInputAndValidate("Enter the value of x: ", 0, 10);
     displayHeader();
 
-    int term_number = 1;
-    bool is_limit_reached = false;
+    //printf("Factorial of 3: %d\n", calculateFactorial(3));
 
-    float sum = 1;
+    //float first = calculateTerm(x, term_number++);
+    //float second = calculateTerm(x, term_number++);
+    //printf("%f %f", first, second);
+
+
     while (!is_limit_reached){
-        float current_term = calculateTerm(x, term_number++);
+        current_term = calculateTerm(x, term_number);
+
         sum += current_term;
         displayRowResult(term_number, current_term);
         is_limit_reached = (absoluteValue(current_term) < THRESHOLD);
         term_number++;
         latest_term = current_term;
-    }
 
+    #ifdef DEBUG
+        printf("Curr Term: %d Sum: %f Is_limit: %b Termnr: %d Latest: %f", current_term, sum, is_limit_reached, term_number, latest_term);
+        printf("Sum: %f\n", sum);
+        while(getchar()!= '\n'){}
+    #endif // DEBUG
+
+    }
     return 0;
 }
 
-void absoluteValue(float current_term){
+
+float absoluteValue(float current_term){
     if (current_term < 0){
         return (current_term * -1);
     }else return current_term;
@@ -65,15 +88,26 @@ float calculateTerm(int x, int term_number){
 
     float nominator = power(x, term_number);
     int factorial = calculateFactorial(term_number);
-    float product = ((float)nominator / factorial);
-    return product;
+    float quotient = (nominator / factorial);
+
+    #ifdef DEBUG
+        printf("Term-value: %f ", quotient);
+        printf("Nom: %f ", nominator);
+        printf("Fact: %d ", factorial);
+        printf("Quo: %f ", quotient);
+    #endif
+
+    return quotient;
 }
 
 
 int calculateFactorial(int term){
 
     int factorial = 1;
-    while(term-- > 1) factorial *= term;
+    while(term > 1) {
+        factorial *= term;
+        term--;
+    }
     return factorial;
 }
 
--- PracticeExamples/main.c
+++ PracticeExamples/main.c
@@ -4,10 +4,19 @@
 int main()
 {
     //size();
-    strings();
-    enumerations();
-    printf("%d", square(5));
-    printf("%d", inline_square(5));
+
+    int a = 3;
+    int b = 4;
+    float c;
+
+    c = (float)a/b;
+    printf("%f", c);
+
+
+    //strings();
+    //enumerations();
+    //printf("%d", square(5));
+    //printf("%d", inline_square(5));
 
     return 0;
 }
